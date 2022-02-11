ee17c05d163bf34fbeb946ed65dbe7360c30f8b3
zhuk-zhenya <88724582+zhuk-zhenya@users.noreply.github.com>
2022-02-11 11:14:39 +0300

    Add Bootstrap

--- myawesomeblog/urls.py
+++ myawesomeblog/urls.py
@@ -17,7 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+import events.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', events.views.home, name='home'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
